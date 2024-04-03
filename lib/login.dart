5cbafc1cb9d57e34132d04a58a727adea7496084
icicle_4 <dwayne.han4@gmail.com>
2024-04-03 19:53:47 +0900

    feat: main, login screen 추가

---
+++ lib/login.dart
@@ -0,0 +1,77 @@
+﻿import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () {
+                  signInWithGoogle();
+                },
+                child: Card(
+                  margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                  elevation: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/images/google.png'),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text(
+                        'Sign IN with Google',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+Future<void> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  await FirebaseAuth.instance.signInWithCredential(credential).then((value) {
+    debugPrint(value.user?.email);
+  }).onError((error, stackTrace) {
+    debugPrint('error : $error');
+  });
+}
